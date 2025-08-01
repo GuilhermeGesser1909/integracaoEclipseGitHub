4703cf62b3c12f6dd9b655ec0ab2c42f9bb3341c
GuilhermeGesser1909 <gesser608@gmail.com>
2025-08-01 11:22:04 -0300

    Update TesteIntegracao.java

--- ProjetoEclipseGitHub/src/TesteIntegracao.java
+++ ProjetoEclipseGitHub/src/TesteIntegracao.java
@@ -6,6 +6,7 @@ public class TesteIntegracao {
 	System.out.println("Primeira Integração");
 	System.out.println("Segunda Integração");
 	System.out.println("Terceira Integração");
+	System.out.println("fazendo alteracao no git");
 	}
 
 }
